b4775241420f379ae70d29bc09d28fcc0a9288c2
aslenok <130287435+aslenok@users.noreply.github.com>
2023-04-09 16:51:59 +0500

    Create itswrong.py

---
+++ itswrong.py
@@ -0,0 +1,11 @@
+print(Hello, world!)
+a = input('Введи 1ое число)
+if a = 5:
+print('Прекрасно')
+attempts == input('введи число сложений')
+d == O          
+for i in range attempts:
+          c = int(intup('введи слогаемое)
+          d += c
+print 'результат =' + str(d)
+
